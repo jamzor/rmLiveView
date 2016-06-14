82d406eef27a7166debd4d2a2ab0ba07ad56a957
jamzor <james@jmproj.com>
2016-06-14 15:43:16 -0600

    Added more test markers and made some fixes to make the front end look more professional. Will have to wait to hear about the router device ticket to move forwards on the GPS data

--- lib/testdata.sql
+++ lib/testdata.sql
@@ -1,1 +1,7 @@
-INSERT INTO Marker (name,latitude,longitude,link) VALUES ('Calgary', 51.0486, -114.0708, 'http://www.calgary.ca')
+/*INSERT INTO Marker (name,latitude,longitude,link) VALUES ('Calgary', 51.0486, -114.0708, 'http://www.calgary.ca')*/
+INSERT INTO Marker (name,latitude,longitude,link) VALUES ('Edmonton', 53.5557952, -113.6343729, 'http://www.edmonton.ca');
+INSERT INTO Marker (name,latitude,longitude,link) VALUES ('Grande Prairie', 55.1597369, -118.8906059, 'http://www.cityofgp.ca');
+INSERT INTO Marker (name,latitude,longitude,link) VALUES ('Fort McMurray', 56.704919, -111.3705794, 'http://www.fortmcmurraytourism.com');
+INSERT INTO Marker (name,latitude,longitude,link) VALUES ('High Level', 58.5066893, -117.1660949, 'http://www.highlevel.ca');
+INSERT INTO Marker (name,latitude,longitude,link) VALUES ('Red Deer', 52.2666952, -113.8743034, 'http://www.reddeer.ca');
+INSERT INTO Marker (name,latitude,longitude,link) VALUES ('Lethbridge', 49.6880468, -112.9162765, 'http://www.lethbridge.ca');
