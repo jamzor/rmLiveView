31e6cfed88557fa1a1ba6512fc3424a8c96726fa
jamzor <james@jmproj.com>
2016-06-17 08:21:57 -0600

    Added the jquery library and got the page running an AJAX script every 10 seconds. It is not yet properly updating the markers though.

--- flaskapp.py
+++ flaskapp.py
@@ -19,12 +19,18 @@ def liveview():
     if request.method == 'GET':
         markers = json.dumps(getMarkers(), indent=2)
         if markers:
-            print markers
             return render_template('mapscripts.html', markers=markers), 200
         return render_template('mapscripts.html'), 200
     else:
         # POST METHOD
         return render_template('mapscripts.html'), 200
+
+@flask_app.route('/_query_db')
+def query_db():
+    markers = json.dumps(getMarkers(), indent=2)
+    if markers:
+        return markers
+    return None
 
 @flask_app.route('/<path:path>')
 def catch_all(path):
