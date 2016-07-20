011788583bda4161ec0dc17982020484e4d4c095
jamzor <james@jmproj.com>
2016-07-20 16:58:26 -0600

    Made some changes to add password hashing with SHA256 for encryption. Still needs to be tested and fixed accordinly

--- authentication.py
+++ authentication.py
@@ -16,7 +16,32 @@
 #               central spot to make changes to libraries
 #****************************************************************************
 
+from database import DBMethods
 
+import hashlib
+import binascii	#These are used to implement a PKCS#5 pw-based key der func
+from base64 import b64encode
+from os import urandom
+
+
+#****************************************************************************
+#   USER GENERATION METHODS
+#   @author:    james macisaac
+#   @desc:      These methods handle the creation of user accounts.
+#****************************************************************************
+
+class MasterGeneration():
+	@classmethod
+	def createMasterAcc(self, username, pw, email, enable):
+		# hash the password
+		
+		pw_hash, salt = Security.generateHash(pw) # calls the crypto methods
+		
+		result, status = DBMethods.addNewMaster(username, pw_hash, salt, email, True)
+		print status
+		if result:
+			return True
+		return False
 
 #****************************************************************************
 #   USER METHODS
@@ -26,7 +51,6 @@
 #****************************************************************************
 
 class UserMethods():
-	logged_in_users = []
 
 	def logInUser():
 		pass
@@ -38,7 +62,31 @@ class UserMethods():
 #               of things for the User accounts.
 #****************************************************************************
 
-class Security():
+HASH_ALGORITHM = 'sha256'
+ROUNDS = 100000
 
-	def placeholder():
-		pass
+class Security():
+	@classmethod
+	def genSalt():
+		rgn = os.urandom(64)
+		salt = b64encode(rgn).decode('utf-8') # 64 byte OS RGN salt for hash
+		return salt
+
+	@classmethod
+	def generateHash(self, pw, salt=None):
+		pass_enc = pw.encode()
+		if salt:
+			salt_enc = salt.encode()
+		else:
+			salt_enc = genSalt()
+		hash = binascii.hexlify(hashlib.pbkdf2_hmac(HASH_ALGORITHM, pass_enc, salt_enc, ROUNDS))
+		hash_dec = hash.decode()
+		return hash_dec, salt_enc.decode()
+		
+	@classmethod
+	def checkHash(self, pw, salt, hash):
+		new_hash, new_salt = generateHash(pw,salt)
+		if hash == new_hash:
+			return True
+		else:
+			return False
